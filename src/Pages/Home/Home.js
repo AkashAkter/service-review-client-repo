5293873bb5381ad2dbaf6c3286cef17f28c1d871
Akash Akter <AkashAkter>
2022-11-07 22:30:19 +0600

    navbar added

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            Whereas recognition of the inherent dignity
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA5S-9kSWyvLu81a36QI0LGbEIR6GfTnnw",
+    authDomain: "service-review-2b58a.firebaseapp.com",
+    projectId: "service-review-2b58a",
+    storageBucket: "service-review-2b58a.appspot.com",
+    messagingSenderId: "973882914223",
+    appId: "1:973882914223:web:334abe936215c644d16b5a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
