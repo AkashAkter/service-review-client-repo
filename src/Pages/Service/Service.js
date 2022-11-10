7fd591c8d67a3f3f58240b59fb79c2b29a01069b
Akash Akter <AkashAkter>
2022-11-10 23:24:07 +0600

    All Done

--- src/Pages/Service/Service.js
+++ src/Pages/Service/Service.js
@@ -10,11 +10,20 @@ const Service = ({ service }) => {
         <div>
             <div className="card shadow-2xl shadow-indigo-400/20 m-6 h-96">
                 <figure className="px-10 pt-10">
-                    <PhotoProvider>
-                        <PhotoView src={image}>
-                            <img src={image} alt="Shoes" className="shadow-indigo-400/20 h-40 w-60 rounded-xl" />
-                        </PhotoView>
-                    </PhotoProvider>
+                    {
+                        image ?
+                            <PhotoProvider>
+                                <PhotoView src={image}>
+                                    <img src={image} alt="Shoes" className="shadow-indigo-400/20 h-40 w-60 rounded-xl" />
+                                </PhotoView>
+                            </PhotoProvider>
+                            :
+                            <PhotoProvider>
+                                <PhotoView src="https://images.squarespace-cdn.com/content/v1/5430da84e4b096aeb94a0050/1425876678608-3UIKARYPCKAQUFL18RZX/parrots.jpg?format=1000w">
+                                    <img src="https://images.squarespace-cdn.com/content/v1/5430da84e4b096aeb94a0050/1425876678608-3UIKARYPCKAQUFL18RZX/parrots.jpg?format=1000w" alt="Shoes" className="shadow-indigo-400/20 h-40 w-60 rounded-xl" />
+                                </PhotoView>
+                            </PhotoProvider>
+                    }
                 </figure>
                 <div className="card-body items-center">
                     <h2 className="card-title">{serviceName}</h2>
